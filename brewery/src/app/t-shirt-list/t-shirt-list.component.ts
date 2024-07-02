83a4bf7faeaa0d8e5eb7f5764367209eec1caaee
Esteban Orellano <esteban.orellano.912@gmail.com>
2024-07-01 22:12:50 -0300

    Directivas

---
+++ brewery/src/app/t-shirt-list/t-shirt-list.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { Tshirt } from './t-shirt';
+
+@Component({
+  selector: 'app-t-shirt-list',
+  templateUrl: './t-shirt-list.component.html',
+  styleUrl: './t-shirt-list.component.css'
+})
+export class TShirtListComponent {
+  tshirts: Tshirt[] = [
+    {
+      "image": "assets/img/river96.jpg",
+      "name": "River Plate",
+      "year": 1996,
+      "price": 2000,
+      "stock": 0,
+      "clearance" : false,
+    },
+    {
+      "image": "assets/img/boca00.jpg",
+      "name": "Boca Juniors",
+      "year": 2000,
+      "price": 9120,
+      "stock": 26,
+      "clearance" : true,
+    },
+    {
+      "image": "assets/img/cai02.jpg",
+      "name": "Independiente",
+      "year": 2002,
+      "price": 1500,
+      "stock": 22,
+      "clearance" : false,
+    },
+    {
+      "image": "assets/img/racing01.jpg",
+      "name": "Racing Club",
+      "year": 2001,
+      "price": 2100,
+      "stock": 22,
+      "clearance" : false,
+    },
+    {
+      "image": "assets/img/casla14.jpg",
+      "name": "San Lorenzo",
+      "year": 2014,
+      "price": 3200,
+      "stock": 100,
+      "clearance" : false,
+    }
+  ];
+}
---
+++ brewery/src/app/t-shirt-list/t-shirt.ts
@@ -0,0 +1,8 @@
+export interface Tshirt{
+    image: string;
+    name : string;
+    year : number;
+    price : number;
+    stock : number;
+    clearance : boolean;
+}
